4b29aae1281798922fc29a31e759ea8fe98e5d06
Pedro Henrique Correa Mota da Silva <pedrohcms2018@gmail.com>
2019-11-18 08:05:07 -0300

    Atualizações

--- autentica.py
+++ autentica.py
@@ -1,12 +1,11 @@
 #coding: utf8
-from tkinter import* #importa toda a biblioteca
-import tkinter
+import tkinter as tk
 import os
 import cv2
 import intelligence
 
 def autentica_Click():
-
+    
     cap = cv2.VideoCapture(0)
 
     while True:
@@ -30,19 +29,19 @@ def autentica_Click():
 def redirect_user(user):
     print(user)
 
-janela = tkinter.Tk()
+janela = tk.Tk()
 janela.title("Sistema de Autenticação") #titulo janela
 #janela["bg"] = "null"  #background
 
 janela.geometry("400x500+500+100")# Organização da janela lxA+E+T
 
 #================LABEL===============
-lb = Label (janela, text="Para entrar, tire a foto")
+lb = tk.Label(janela, text="Para entrar, tire a foto")
 lb.place(x=130, y=150)
 #================/LABEL==============
 
 #=====================BOTÃO========================
-bt = Button(janela, width = 50, text = "Analisar Foto", command = autentica_Click)
+bt = tk.Button(janela, width = 50, text = "Analisar Foto", command = autentica_Click)
 bt.place(x=20, y=200)
 #=====================/BOTÃO=======================
 janela.mainloop()
