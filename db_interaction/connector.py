9b72f449949ec108ee3c4bf8bb465194d2f690cd
pedrohcms <pedrohcms2018@gmail.com>
2019-11-18 01:00:43 -0300

    Atualizações em interfaces gráficas, módulo intelligence, e requeriments
    -Adicionado arquivo __init__.py para demostrar ao python que temos um package.
    -Arquivo CRUD for modificado para crud
    -Módulo intelligence agora tem a função recognize_user para fazer o reconhecimento do usuário.
    -Arquivo requirements foi atualizado para as depedências para o banco de dados.
    -Adicionado arquivo que cria a tabela de usuários no banco de dados.
    -Arquivo connector modificado, para guardar a conexão com o banco de dados.

--- db_interaction/connector.py
+++ db_interaction/connector.py
@@ -1,10 +1,16 @@
 import psycopg2
 
-DB_NAME = "fwkzpiva"
-DB_USER = "fwkzpiva"
-DB_PASS = "bt89yZ5tQ1Lf26ra_g-F-bYmPZT9_DoJ"
+DB_NAME = "chmahdbn"
+DB_USER = "chmahdbn"
+DB_PASS = "5Mi_DUa8WTwqrYPxExWLhb59Iegz8QU2"
 DB_HOST = "motty.db.elephantsql.com"
 DB_PORT = "5432"
+
+#DB_NAME = "db_reconhecimento_facial"
+#DB_USER = "postgres"
+#DB_PASS = "cdc123"
+#DB_HOST = "127.0.0.1"
+#DB_PORT = "5432"
 
 try:
     conn = psycopg2.connect(database = DB_NAME, user = DB_USER, password = DB_PASS, host = DB_HOST, port = DB_PORT)
---
+++ db_interaction/create_users_table.py
@@ -0,0 +1,19 @@
+from psycopg2 import DatabaseError
+from db_interaction.connector import conn
+
+cur = conn.cursor()
+
+try:
+    cur.execute("""CREATE TABLE userdata (
+                    id SERIAL PRIMARY KEY, 
+                    user_name TEXT, 
+                    access_lvl INTEGER,
+                    label TEXT
+                )
+            """)
+    cur.close()
+    conn.commit()
+    conn.close()
+
+except(Exception, DatabaseError) as error:
+    print(error)
