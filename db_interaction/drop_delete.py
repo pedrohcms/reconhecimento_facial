abbf0f4891039fa482509f458c3a539ca6d81c9b
Gustavo A M Costa <51254837+gustavoamc@users.noreply.github.com>
2019-11-23 11:53:05 -0300

    Correção erro drop_delete
    Correção erro drop_delete

--- db_interaction/drop_delete.py
+++ db_interaction/drop_delete.py
@@ -5,12 +5,11 @@ cur = conn.cursor()
 
 def drop_table():
     try:
-        cur.execute("DROP TABLE IF EXISTS table_name CASCADE;")
+        cur.execute("DROP TABLE IF EXISTS userdata CASCADE;")
         print("Executed!")
-        conn.commit
-    except (expression,DatabaseError) as error:
+        conn.commit()
+    except (Exception,DatabaseError) as error:
         print(error)
-
 
 #"CASCADE" will drop the dependent objects of the table
 #For the next commit i will check the need for a close cursor function call here
