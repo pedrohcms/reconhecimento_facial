a4421aead5ab7b2c8b24d8b0b55b5f0c01354ae1
unknown <pedrohcms2018@gmail.com>
2019-12-21 17:04:49 -0300

    Modificações em User.py

--- db_interaction/User.py
+++ db_interaction/User.py
@@ -1,5 +1,6 @@
 from db_interaction.Connector import Connector
 
+
 class User(Connector):
 
     def __init__(self):
@@ -7,10 +8,12 @@ class User(Connector):
         self.cursor = self.conn.cursor()
 
     def insert(self, p_user_name, p_access_lvl, p_label):
-        self.cursor.execute("INSERT INTO userdata (user_name, access_lvl, label) VALUES(%s,%s,%s);",(p_user_name, p_access_lvl, p_label))
-        
+        self.cursor.execute(
+            "INSERT INTO userdata (user_name, access_lvl, label) VALUES(%s,%s,%s);", (p_user_name, p_access_lvl, p_label))
+
     def select(self, p_label):
-        self.cursor.execute("Select * from userdata where label like %s;",(p_label,))
+        self.cursor.execute(
+            "Select * from userdata where label like %s;", (p_label,))
 
         records = cursor.fetchall()
 
